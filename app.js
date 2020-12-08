2ce19c23d4d68618762c6f702795f2da49d735d8
Bhavnesh sharma <bhanusharma252001@gmail.com>
2020-12-08 10:13:11 +0530

    Port Updated

--- app.js
+++ app.js
@@ -159,8 +159,12 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-app.listen(port, function() {
-  console.log("Server started sucessfully");
-});
+app.listen(process.env.PORT||3000,()=>
+{
+    console.log("Server started on Port 3000");
+})
+
+// app.listen(port, function() {
+//   console.log("Server started sucessfully");
+// });
